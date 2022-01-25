63e3b2cc9f9cbb16c9412e7d3a539eb78f208316
Kiana Henny <kianah@zulu.uwyo.edu>
2022-01-25 09:24:41 -0700

    pointsize fix

--- week2/strucfits_code.py
+++ week2/strucfits_code.py
@@ -5,7 +5,7 @@ from astropy.table import Table
 objs = Table.read("/d/scratch/ASTR5160/week2/struc.fits")
 plt.scatter(objs["RA"], objs["DEC"], s=2, marker = "o", color="blue")
 ii = objs["EXTINCTION"][:,0]>=.22
-plt.scatter(objs[ii]["RA"], objs[ii]["DEC"], color="purple", s=1, marker = "o")
+plt.scatter(objs[ii]["RA"], objs[ii]["DEC"], color="purple", s=2, marker = "o")
 saveloc = "/d/www/kianah/public_html/week2_pos_scatter2.png"
 plt.savefig(saveloc)
 plt.clf()
