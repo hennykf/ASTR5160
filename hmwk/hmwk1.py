b95898264f2df8cd2810a4cf27a09e0a7df8ce7e
Adam U.W. Myers <amyers14@uwyo.edu>
2022-02-21 19:22:10 -0700

    Some examples of how to make the homework code more compact (and accurate)

--- hmwk/hmwk1.py
+++ hmwk/hmwk1.py
@@ -1,11 +1,15 @@
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 from astropy.table import Table
 from astropy.coordinates import SkyCoord, EarthLocation, AltAz
-from  astropy.time import Time
+import astropy.units as u
+from astropy.time import Time
 from calendar import monthrange
 import argparse
+
+# ADM just hard-coding the offset between UTC and MST...
+utc_off = 7*u.hour
+# ADM ...and the filename.
+filename = "/d/scratch/ASTR5160/week4/HW1quasarfile.txt"
 
 def best_airmass(month):
     """
@@ -20,9 +24,7 @@ def best_airmass(month):
     An astropy Table containing the date, quasar position, and airmasses of the lowest-airmass
     quasar each night of that month.
     """
-    month = int(month)
-    quasars = Table.read('/d/scratch/ASTR5160/week4/HW1quasarfile.txt', format='ascii.no_header',
-                         names = ['position'])
+    quasars = Table.read(filename, format='ascii.no_header', names=['position'])
     
     # KFH Separate RA and dec and convert into degrees
     ra = ['{}h{}m{}s'.format(q[0:2], q[2:4], q[4:9]) for q in quasars['position']]
@@ -36,33 +38,34 @@ def best_airmass(month):
     month_num, day_num = monthrange(2022, month)
     days = np.arange(1, day_num+1)
     
-    times = []
-    for i in days:
-        if i == 1:
-            next_month = month + 1
-            time = '2022-' + str('%02d' % next_month) + '-' + str('%02d' % i) + 'T06:00:00.000'
-        else:
-            time = '2022-' + str('%02d' % month) + '-' + str('%02d' % i) + 'T06:00:00.000'
-        times.append(Time(time))
+    # ADM More compact way of constructing times.
+    t = ['2022-' + str(month) + '-' + str(i) + ' 23:00:00' for i in days]
+    times = Time(t, format='iso') + utc_off
 
     # KFH Find alt and az of quasars at 11 PM MST on each day of the month
     kpno = EarthLocation.of_site('kpno')
-    loc_altaz = [coords.transform_to(AltAz(location=kpno, obstime=t)) for t in times]
 
-    # KFH Find airmass of all objects
-    airmass = np.asarray([1/np.cos(90 -t.alt.degree) for t in loc_altaz])
+    # ADM find the airmasses at the given times in just one step.
+    airmass = np.vstack(
+        [coords.transform_to(AltAz(location=kpno, obstime=t)).secz.value
+         for t in times])
 
-
-    low_am = np.argmin(np.abs(airmass),1)
-    low_airmasses =  quasars[low_am]
+    # ADM find the index of the lowest airmass, first ensuring that 
+    # ADM negative numbers are converted to a very-high airmass.
+    ii = np.argmin(np.where(airmass > 0, airmass, 1e16), 1)
+    low_am = np.min(np.where(airmass > 0, airmass, 1e16), 1)
+    low_qs = quasars[ii]
 
     # KFH Put all vals into final table of the lowest-airmass objs each night in a month
-    final_tab = Table([times, low_airmasses['position'], low_airmasses['ra'], low_airmasses['dec'],np.min(np.abs(airmass),1)],
-                  names=['Date', 'Quasar Coordinates', 'RA', 'Dec', 'Airmass'])
+    # ADM just to be illustrative, I changed the times back into MST instead of UTC.
+    final_tab = Table(
+        [times-utc_off, low_qs['position'], low_qs['ra'], low_qs['dec'], low_am],
+        names=['Date (MST)', 'Quasar Coordinates', 'RA', 'Dec', 'Airmass'])
+
     return(final_tab)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Enter an integer corresponding to a month of the year')
-    parser.add_argument('month')
+    parser.add_argument('month', type=int)
     args = parser.parse_args()
     print(best_airmass(args.month))
