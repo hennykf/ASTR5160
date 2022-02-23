fa6a0500fcaaff4720f7f420d34bcb923cf6d41a
Kiana Henny <kianah@shoshone.uwyo.edu>
2022-02-23 11:14:38 -0700

    wrote caps function

--- week5/spherical_caps.py
+++ week5/spherical_caps.py
@@ -16,7 +16,6 @@ def ra_vect_array(ra):
     # KFH Calculate the cartesian coordinates of spherical cap
     area = SkyCoord(ra=new_ra, dec = '+00d00m00s', frame='icrs').cartesian
     vec4array=[area.x.value, area.y.value, area.z.value, 1]
-    print("RA Vector 4-array: " + str(vec4array))
     return(vec4array)
 
 def dec_vect_array(dec):
@@ -31,8 +30,19 @@ def dec_vect_array(dec):
     area = SkyCoord(ra=0*u.degree, dec = 90*u.degree, frame='icrs').cartesian
     h = 1 - np.sin(int(dec[1:3])*np.pi/180)
     vec4array=[area.x.value, area.y.value, area.z.value, h]
-    print("Dec Vector 4-array: " + str(vec4array))
     return(vec4array)
 
-ra_vect_array('05h00m00s')
-dec_vect_array('+36d00m00s')
+def cap(ra, dec, radius):
+    """
+    Function to convert an RA and dec into a spherical cap vector 4-array
+    in cartesian coordinates, centered around the input RA,dec.
+    """
+    area = SkyCoord(ra, dec, frame='icrs').cartesian
+    h = 1-np.cos(radius*np.pi/180)
+    vec4array=[area.x.value, area.y.value, area.z.value, h]
+    return(vec4array)
+
+if __name__ == '__main__':
+    ra_vect_array('05h00m00s')
+    dec_vect_array('+36d00m00s')
+    cap('05h00m00s', '+36d00m00s', 1)
