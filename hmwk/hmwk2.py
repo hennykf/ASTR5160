87257046a0ebfdbcf492b30d9e3511c96ceb00da
Kiana Henny <kianah@shoshone.uwyo.edu>
2022-03-04 15:22:43 -0700

    push inits

--- hmwk/hmwk2.py
+++ hmwk/hmwk2.py
@@ -1,2 +1,12 @@
 import  numpy as np
-from tasks.week6.spherical_caps import ra_vect
+from tasks.week6.spherical_caps import ra_vect_array, dec_vect_array
+
+def rect_area(ra_min, ra_max, dec_min, dec_max):
+    """
+    Stuff
+    """
+
+    # KFH First we'll find the area bounded by these four boundaries
+    
+    
+    
